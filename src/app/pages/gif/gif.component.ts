7a1e53ea6ba5e2e1bf67cee5d81d5fc5f2f594ff
Arnau B. <arnau_b17@hotmail.com>
2024-08-08 17:15:42 +0200

    search bar in the gifComponent

--- src/app/pages/gif/gif.component.ts
+++ src/app/pages/gif/gif.component.ts
@@ -4,17 +4,21 @@ import { GifService } from '../../service/gif.service';
 import { Gif } from '../../interface/gif';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../../components/card/card.component';
+import { InputComponent } from '../../components/input/input.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-gif',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, InputComponent, FormsModule],
   templateUrl: './gif.component.html',
   styleUrl: './gif.component.css',
 })
 export class GifComponent implements OnInit {
   public gifResults$!: Observable<Gif[]>;
   public errorMessage!: string;
+  searchTerm: string = '';
   constructor(private service: GifService) {}
 
   ngOnInit(): void {
@@ -28,5 +32,9 @@ export class GifComponent implements OnInit {
         return EMPTY;
       })
     );
+  }
+  onSearch() {
+    this.searchGifs(this.searchTerm);
+    this.searchTerm = '';
   }
 }
